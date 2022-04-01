949e1056c65a83f9e187402c527fabf45bb1109a
TA2k <tombox2020@gmail.com>
2022-04-01 21:49:40 +0200

    0.0.4

--- main.js
+++ main.js
@@ -30,8 +30,15 @@ class Hekr extends utils.Adapter {
         this.deviceDict = {};
         this.deviceMode = {};
         this.devicelanIp = {};
+//Neu Anfang
+        this.devicemodel = {};
+        this.check = null;
+        this.c_update = 0;
+//Neu Ende
         this.session = {};
-        this.tplkkl = { sw: 2, light_Sw: 3, speed: 4, tm_Minutes: 5, cleaning: 6, R: 7, B: 7, G: 7, rgb: 7, hour: 8, minute: 8, second: 8, time: 8 };
+//Geändert Anfang
+        this.tplkkl = { sw: 2, light_Sw: 3, speed: 4, tm_Minutes: 5, cleaning: 6, R: 7, B: 7, G: 7, rgb: 7, hour: 8, minute: 8, second: 8, time: 8, RGB_Sw: 9, raw_manually: 20 };
+//Geändert Ende
     }
 
     /**
@@ -113,6 +120,9 @@ class Hekr extends utils.Adapter {
                     this.deviceDict[device.devTid] = device.ctrlKey;
                     this.deviceMode[device.devTid] = device.workModeType;
                     this.devicelanIp[device.devTid] = device.lanIp;
+//Neu Anfang
+                    this.devicemodel[device.devTid] = device.model;
+//Neu Ende
                     const productPubKey = device.productPublicKey;
                     const DevID = device.devTid;
                     await this.setObjectNotExistsAsync(device.devTid, {
@@ -153,7 +163,21 @@ class Hekr extends utils.Adapter {
                         },
                         native: {},
                     });
-
+//Neu Anfang
+                    await this.setObjectNotExists(device.devTid + ".status.raw_manually", {
+                        //RAW versenden
+                        type: "state",
+                        common: {
+                            name: "raw_manually",
+                            type: "string",
+                            read: true,
+                            write: true,
+                            role: "indicator.status",
+                            desc: "Sent manually raw",
+                        },
+                        native: {},
+                    });
+//Neu Ende
                     await this.setObjectNotExists(device.devTid + ".status.time", {
                         //Button fÃ¼r aktuelle Zeit senden
                         type: "state",
@@ -223,6 +247,9 @@ class Hekr extends utils.Adapter {
         };
         this.ws.on("open", () => {
             //this.log.debug("WS received:" + message);
+//Neu Anfang
+            this.setState("info.connection", true, true);
+//Neu Ende
             try {
                 this.log.debug("WS open");
                 this.ws.send(JSON.stringify(this.wsAuthMessage));
@@ -253,10 +280,22 @@ class Hekr extends utils.Adapter {
 
                 if (jsonMessage.action === "devSend") {
                     const params = jsonMessage.params;
-                    if (Object.keys(params.data).length > 1) {
-                        this.json2iob.parse(params.devTid + ".status", params.data, { write: true });
+//Neu Anfang
+                    if (this.c_update === 0) this.check = params.data;
+                    ++this.c_update;
+//Neu Ende
+//Geändert Anfang
+                    if (Object.keys(params.data).length > 1 && this.c_update > 3) {
                         this.log.debug("WS received:" + message);
+                        this.c_update = 1;
+                        Object.keys(params.data).forEach((n) => {
+                            if (this.check[n] !== params.data[n])
+                                this.setState(params.devTid + ".status." + n, params.data[n], true);
+                        });
+                        this.check = params.data;
+                        //this.json2iob.parse(params.devTid + ".status", params.data, { write: true });
                     }
+//Geändert Ende
                     // if (params.data.raw) {
                     //     for (let n = 0; n < params.data.raw.length; n += 2) {
                     //         const index = n / 2;
@@ -278,7 +317,7 @@ class Hekr extends utils.Adapter {
                     this.log.debug("WS received:" + message);
                 }
             } catch (error) {
-                this.log.error(error);
+                this.log.error("Test: " + error);
             }
         });
 
@@ -331,7 +370,10 @@ class Hekr extends utils.Adapter {
                 const ctrlKey = this.deviceDict[deviceId];
                 const workMode = this.deviceMode[deviceId];
                 const lanIp = this.devicelanIp[deviceId];
-
+//Neu Anfang
+                const model = this.devicemodel[deviceId];
+                this.log.debug("Model: " + model);
+//Neu Ende
                 if (command == "B" || command == "G" || command == "R") {
                     this.log.info("For color use datapoint rgb");
                     return;
@@ -343,21 +385,31 @@ class Hekr extends utils.Adapter {
                 }
 
                 rawString = this.tplkkl[command];
-
-                if (command == "rgb") {
-                    if (state.val != "ffffff" && state.val != "000000") {
-                        let rgbstr = state.val;
-                        if (rgbstr && rgbstr.length == 7) {
-                            rgbstr = rgbstr.replace("#", "");
-                        } else if (rgbstr && rgbstr.length != 6) {
-                            this.log.info("The color " + command + " is not a hex");
-                            return;
-                        }
-                        rawString = "48090200" + this.DecToHex(rawString) + rgbstr + "00";
+//Geändert Anfang
+                if (command === "raw_manually") {
+                    const checkRaw = await this.checkObject(this.namespace + "." + deviceId + ".status.raw");
+                    if (checkRaw === 1) {
+                        const devRaw  = await this.getStateAsync(deviceId + ".status.raw");
+                        rawString = state.val;
+                        //this.log.debug("RAW: " + "4810010A010000000000009B726D00DE".replace(/(.{13}).{2}/,"$112"));
                     } else {
-                        this.log.info("Only colors are allowed");
+                        this.log.info("Cannot read dp raw");
                         return;
                     }
+                } else if (command === "RGB_Sw" && (state.val === 1 || state.val === 0)) {
+                    this.log.debug("Command RGB_Sw: " + state.val);
+                    this.log.info("Command " + command + " not implemented");
+                    return;
+                } else if (command === "rgb") {
+                    let rgbstr = state.val;
+                    if (rgbstr && rgbstr.length == 7) {
+                        rgbstr = rgbstr.replace("#", "");
+                    } else if (rgbstr && rgbstr.length != 6) {
+                        this.log.info("The color " + command + " is not a hex");
+                        return;
+                    }
+                    rawString = "48090200" + this.DecToHex(rawString) + rgbstr + "00";
+//Geändert Ende
                 } else if (command == "time") {
                     const a = new Date();
                     rawString = "48090200" + this.DecToHex(rawString) + this.DecToHex(a.getHours()) + this.DecToHex(a.getMinutes()) + this.DecToHex(a.getSeconds()) + "00";
@@ -421,6 +473,28 @@ class Hekr extends utils.Adapter {
             }
         }
     }
+//Neu Anfang
+    /**
+     * Is channel or device exists
+     * @param {string} channel => path
+     */
+    checkObject(channel) {
+        return new Promise(resolve => {
+            this.getForeignObjects(channel,(err, obj) => {
+                if (err) {
+                    this.log.debug("Read Object: " + err);
+                    resolve(0);
+                } else {
+                    try {
+                        resolve(1);
+                    } catch (e) {
+                        resolve(0);
+                    }
+                }
+            });
+        });
+    }
+//Neu Ende
 }
 
 if (require.main !== module) {
